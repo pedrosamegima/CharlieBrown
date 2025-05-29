49c5ec5662f2c924c3c75bd48bde9c77d06e5925
Pedro Henrique <136722484+pedrosamegima@users.noreply.github.com>
2025-05-28 23:21:35 -0300

    Update entrar.js

--- js/entrar.js
+++ js/entrar.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function(){
         const cinema = document.getElementById("cinema").value;
         const password = document.getElementById("password").value;
 
-        if(nome === "Millene Gabrielle Andrade de Jesus" && cinema === "03/05/2025" && password === "DonaDoMeuPensamento"){
+        if(nome === "Dona do meu pensamento" && cinema === "03/05/2025" && password === "DonaEncreca"){
             window.location.href = "nossaPlay.html";
         } else {
             alert("Você não tem permissão pra ir para a próxima página");
