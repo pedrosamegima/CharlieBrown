94c7048689c6afb5ec93866dfa54b034325138ed
Pedro Henrique <136722484+pedrosamegima@users.noreply.github.com>
2025-06-08 12:44:57 -0300

    m

--- js/entrar.js
+++ js/entrar.js
@@ -4,8 +4,8 @@ document.addEventListener("DOMContentLoaded", function () {
     loginForm.addEventListener("submit", function (event) { // <-- aqui
         event.preventDefault();
 
-        const nome = document.getElementById("nome").value;
-        const password = document.getElementById("password").value;
+        const nome = document.getElementById("nome").value.trim();
+        const password = document.getElementById("password").value.trim();
 
         if (nome === "Millene" && password === "030525") {
             window.location.href = "nossaPlay.html";
