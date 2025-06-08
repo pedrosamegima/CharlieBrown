92fea8f84b6598c04c575598733872baf0181d99
Pedro Henrique <136722484+pedrosamegima@users.noreply.github.com>
2025-06-08 13:20:32 -0300

    Switch alert

--- js/entrar.js
+++ js/entrar.js
@@ -8,9 +8,26 @@ document.addEventListener("DOMContentLoaded", function () {
         const password = document.getElementById("password").value.trim();
 
         if (nome === "Millene" && password === "030525") {
-            window.location.href = "nossaPlay.html";
-        } else {
-            alert("Você não tem permissão pra ir para a próxima página");
+            Swal.fire({
+                imageUrl: "img/foto4.jpeg",
+                imageHeight: 600,
+                width: 450,
+                height: 500,
+                background: '#D3D3D3', 
+                imageAlt: "Dona encrenca",
+                confirmButtonText: "Bem-vinda Dona do meu pensamento!",
+                customClass: {
+                    image: 'image-radius',
+                }
+            }).then(() =>{
+                  window.location.href = "nossaPlay.html";
+            });
+           } else {
+            Swal.fire({
+                icon: "error",
+                title: "Acesso negado!",
+                text: "Você não tem permissão pra ir para a próxima página",
+            });
         }
     });
 });
